4f4dde68d958811b0030dfb5777926c3b763275d
Tom Hunter <tshunter@usc.edu>
2017-02-26 21:46:57 -0500

    add feature engineering script, added to Base Random Forest

---
+++ features.py
@@ -0,0 +1,9 @@
+def engineer(df):
+    df["num_photos"] = df["photos"].apply(len)
+    df["num_features"] = df["features"].apply(len)
+    df["num_description_words"] = df["description"].apply(lambda x: len(x.split(" ")))
+    df["created"] = pd.to_datetime(df["created"])
+    df["created_year"] = df["created"].dt.year
+    df["created_month"] = df["created"].dt.month
+    df["created_day"] = df["created"].dt.day
+    return df
