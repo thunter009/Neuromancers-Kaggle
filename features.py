3e9f4e0a3532020a6b1a0f852699e1a1e1b00ade
Tom Hunter <tshunter@usc.edu>
2017-02-27 21:15:01 -0500

    big buildings feature

--- features.py
+++ features.py
@@ -2,11 +2,19 @@ from pandas import to_datetime
 import numpy as np
 import re
 
+
+def scrub(df):
+    """
+    function designed to perform data cleaning on the training data set
+    """
+    df['building_id'].replace(to_replace='0', value=np.nan, inplace=True)
+    df["created"] = to_datetime(df["created"])
+    return df
+
 def engineer(df):
     df["num_photos"] = df["photos"].apply(len)
     df["num_features"] = df["features"].apply(len)
     df["num_description_words"] = df["description"].apply(lambda x: len(x.split(" ")))
-    df["created"] = to_datetime(df["created"])
     df["created_year"] = df["created"].dt.year
     df["created_month"] = df["created"].dt.month
     df["created_day"] = df["created"].dt.day
